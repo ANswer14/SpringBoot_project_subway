e7bb30d0fb5ea67d100dd898b04030b974d2883c
ANswer14 <dldudgns789789@naver.com>
2024-09-06 14:11:27 +0900

    Update and rename UserInterface.java to UserRepository.java

--- SpringBoot_project_subway/frame/src/main/java/com/lyh/frame/repository/UserRepository.java
+++ SpringBoot_project_subway/frame/src/main/java/com/lyh/frame/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.lyh.frame.repository;
 import com.lyh.frame.model.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserInterface extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
